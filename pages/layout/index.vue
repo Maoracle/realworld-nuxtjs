8a47a04099de7f3154871d4746eaf392ac6418f6
Maoracle <13482507290@163.com>
2022-03-29 21:48:45 +0800

    部署更新测试2

--- pages/layout/index.vue
+++ pages/layout/index.vue
@@ -28,21 +28,24 @@
               </nuxt-link>
             </li>
             <li class="nav-item">
-              <nuxt-link
-                class="nav-link"
-                to="/settings"
-              >
+              <nuxt-link class="nav-link" to="/settings">
                 <i class="ion-gear-a"></i>&nbsp;Settings
               </nuxt-link>
             </li>
             <li class="nav-item">
-              <nuxt-link class="nav-link" to="/profile/123">
-                <img
-                  class="user-pic"
-                  :src="user.image"
-                >
-                {{ user.username }}
+              <nuxt-link
+                class="nav-link"
+                :to="{
+                name:'profile',
+                params:{username:user.username}
+              }"
+              >
+                <img :src="user.image" class="user-pic" />
+                {{user.username}}
               </nuxt-link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="javascript:;" @click.prevent="logout">退出</a>
             </li>
           </template>
 
@@ -87,13 +90,22 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-
+import { mapState } from "vuex";
+const Cookie = process.client ? require("js-cookie") : "";
 export default {
-  name: 'LayoutIndex',
+  name: "LayoutIndex",
   computed: {
-    ...mapState(['user'])
-  }
+    ...mapState(["user"]),
+  },
+  methods: {
+    logout() {
+      // 删除客户端缓存数据
+      this.$store.commit("setUser", null);
+      // 删除服务端数据持久化
+      Cookie.set("user", null);
+      this.$router.push("/");
+    },
+  },
 }
 </script>
 
